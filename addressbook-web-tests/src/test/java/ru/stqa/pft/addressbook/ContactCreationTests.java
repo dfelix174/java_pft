8d0f5d71827d90b28d192ef22b9d869795da9d64
dfelix174 <d.felix174@gmail.com>
2021-02-08 01:49:29 +0200

    Сделан Рефакторин для создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -29,45 +29,59 @@ public class ContactCreationTests {
   }
   @Test
   public void testContactCreation() throws Exception {
-    wd.get("http://localhost/addressbook/index.php");
-    wd.findElement(By.linkText("add new")).click();
+    initContactCreation();
+    fillContactForms(new ContactData("Dmytro", "Rudenko", "Ukraine Kiev", "38044", "38063", "38066", "38097", "fel_1@gmail.com", "fel_2@gmail.com", "fel_3@gmail.com", "6", "September", "1989"));
+    submitContactCreation();
+    gotoHomePage();
+  }
+
+  private void gotoHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForms(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Dmytro");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Rudenko");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Ukraine Kiev");
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("38044");
+    wd.findElement(By.name("home")).sendKeys(contactData.getTelhome());
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("38063");
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getTelmobile());
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("38066");
+    wd.findElement(By.name("work")).sendKeys(contactData.getTelwork());
     wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys("38097");
+    wd.findElement(By.name("fax")).sendKeys(contactData.getTelfax());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("fel_1@gmail.com");
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmailfirst());
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("fel_2@gmail.com");
+    wd.findElement(By.name("email2")).sendKeys(contactData.getEmailsecond());
     wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("fel_3@gmail.com");
+    wd.findElement(By.name("email3")).sendKeys(contactData.getEmailthird());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("6");
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
     wd.findElement(By.xpath("//option[@value='6']")).click();
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("September");
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
     wd.findElement(By.xpath("//option[@value='September']")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys("1989");
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    wd.findElement(By.linkText("home")).click();
+    wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
+  }
+
+  private void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,85 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String telhome;
+  private final String telmobile;
+  private final String telwork;
+  private final String telfax;
+  private final String emailfirst;
+  private final String emailsecond;
+  private final String emailthird;
+  private final String bday;
+  private final String bmonth;
+  private final String byear;
+
+  public ContactData(String firstname, String lastname, String address, String telhome, String telmobile, String telwork, String telfax, String emailfirst, String emailsecond, String emailthird, String bday, String bmonth, String byear) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.telhome = telhome;
+    this.telmobile = telmobile;
+    this.telwork = telwork;
+    this.telfax = telfax;
+    this.emailfirst = emailfirst;
+    this.emailsecond = emailsecond;
+    this.emailthird = emailthird;
+    this.bday = bday;
+    this.bmonth = bmonth;
+    this.byear = byear;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getTelhome() {
+    return telhome;
+  }
+
+  public String getTelmobile() {
+    return telmobile;
+  }
+
+  public String getTelwork() {
+    return telwork;
+  }
+
+  public String getTelfax() {
+    return telfax;
+  }
+
+  public String getEmailfirst() {
+    return emailfirst;
+  }
+
+  public String getEmailsecond() {
+    return emailsecond;
+  }
+
+  public String getEmailthird() {
+    return emailthird;
+  }
+
+  public String getBday() {
+    return bday;
+  }
+
+  public String getBmonth() {
+    return bmonth;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+}
