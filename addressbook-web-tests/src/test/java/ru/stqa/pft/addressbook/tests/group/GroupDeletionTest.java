19ae2568f553df2ab7b99847e52eb9790f4ef103
dfelix174 <d.felix174@gmail.com>
2021-02-22 01:34:51 +0200

    Лекция 4.5. Формирование коллекции для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupDeletionTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.util.List;
+
 public class GroupDeletionTest extends TestBase {
 
   @Test
@@ -14,12 +16,12 @@ public class GroupDeletionTest extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "null"));
     }
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
