da697d8847885a121f3d90108e0556d6ed0b0f0f
dfelix174 <d.felix174@gmail.com>
2021-02-22 23:20:44 +0200

    Множества: неупорядоченные коллекции. Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,11 +16,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForms(new GroupData("test7", "test5", "test6"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test7", "test5", "test6");
+    app.getGroupHelper().fillGroupForms(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
