ccebc60d26e8ab48703939071ae9fd3f6269094d
dfelix174 <d.felix174@gmail.com>
2021-02-23 01:42:56 +0200

    Персинг всего имени через By.tagName("tr")

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -80,7 +80,7 @@ public class ContactHelper extends HelperBase {
 
   public List<ContactData> getContactList() {
     List<ContactData> contacts = new ArrayList<ContactData>();
-    List<WebElement> elements = wd.findElements(By.name("entry"));
+    List<WebElement> elements = wd.findElements(By.tagName("tr"));
     for (WebElement element : elements) {
       String firstname = element.getText();
       ContactData contact = new ContactData(firstname, null, null, null, null, null, null, null, null, null, null, null, null, null);
