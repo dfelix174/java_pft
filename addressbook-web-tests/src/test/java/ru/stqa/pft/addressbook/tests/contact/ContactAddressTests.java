acfb09aa65c5e5bc2a6be5621b74f2a261a8896e
dfelix174 <d.felix174@gmail.com>
2021-03-12 22:30:01 +0200

    Добавленны проверки
    Для тестов на сравнение Адресов, телефонов и Емейлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactAddressTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests.contact;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.tests.TestBase;
@@ -9,9 +10,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactAddressTests extends TestBase {
 
+  @BeforeMethod
+  public void ensurePreconditions (){
+    app.goTo().homePage();
+    if(app.contact().all().size() == 0){
+      app.contact().create( new ContactData().withLastname ("Rudenko").withFirstname ("Dmytro").withAddress("Ukraine Kiev")
+              .withTelhome("38044").withTelmobile("38063").withTelwork("38066").withTelfax("38097")
+              .withEmailfirst("fel_1@gmail.com").withEmailsecond("fel_2@gmail.com").withEmailthird("fel_3@gmail.com")
+              .withBday("6").withBmonth("September").withByear("1989").withGroup("test1"),true);
+    }
+    app.goTo().homePage();
+  }
+
   @Test
   public void testContactAddress() {
-    app.goTo().homePage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/contact/ContactPhoneTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests.contact;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.tests.TestBase;
@@ -12,10 +13,21 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class ContactPhoneTests extends TestBase {
 
+  @BeforeMethod
+  public void ensurePreconditions (){
+    app.goTo().homePage();
+    if(app.contact().all().size() == 0){
+      app.contact().create( new ContactData().withLastname ("Rudenko").withFirstname ("Dmytro").withAddress("Ukraine Kiev")
+              .withTelhome("38044").withTelmobile("38063").withTelwork("38066").withTelfax("38097")
+              .withEmailfirst("fel_1@gmail.com").withEmailsecond("fel_2@gmail.com").withEmailthird("fel_3@gmail.com")
+              .withBday("6").withBmonth("September").withByear("1989").withGroup("test1"),true);
+    }
+    app.goTo().homePage();
+  }
+
 
   @Test
   public void testContactPhones() {
-    app.goTo().homePage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
