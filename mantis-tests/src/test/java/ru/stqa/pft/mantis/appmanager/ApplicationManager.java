0232fe83d78e7af93a300eee4af69f4290d29fd4
dfelix174 <d.felix174@gmail.com>
2021-03-29 23:16:58 +0300

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP
    Рабочая версия

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -2,10 +2,10 @@ package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,31 +14,52 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
 
-  public ApplicationManager(String browser) {
+
+  public ApplicationManager(String browser)  {
     this.browser = browser;
     properties = new Properties();
   }
 
   public void init() throws IOException {
-    String target = System.getProperty("target","local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader
+            (new File(String.format("src/test/resources/%s.properties", target))));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
+  }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public Object getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      } else if (browser.equals(BrowserType.EDGE)){
+        wd = new EdgeDriver();
+      }
+
+      wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+      properties.getProperty("web.adminPassword");
+    }
+    return wd;
   }
 }
